c308e94fec43db6b2f6ec4ee06641e981fa672fc
Andrew Gazelka <andrew.gazelka@gmail.com>
2018-03-22 20:00:08 -0500

    Writing your first Django app, part 4

--- 001_mysite/polls/admin.py
+++ 001_mysite/polls/admin.py
@@ -1,5 +1,6 @@
 # Register your models here.
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 admin.site.register(Question)  # Adds question to admin interface
+admin.site.register(Choice)  # Adds question to admin interface
